ca327f12c693d5feda8b9fd51d645231b70049a6
Tom Fogal <tfogal@alumni.unh.edu>
2014-04-10 11:54:33 +0200

    add a place to store some test programs.

---
+++ testprograms/3dloop.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void subfunc(unsigned n) {
+  const char* hello = "hello";
+  const char* world = "world";
+  printf("%s %s\n", hello, world);
+  for(size_t i=0; i < n; ++i) {
+    for(size_t j=0; j < 10; ++j) {
+      for(size_t k=0; k < 20; ++k) {
+      }
+    }
+  }
+}
+int main(int argc, char* argv[]) {
+  if(argc == 2) {
+    subfunc(atoi(argv[1]));
+  }
+  return 0;
+}
