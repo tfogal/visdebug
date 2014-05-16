d8f6f66a709e56951a4afaf1c295dee000feb153
Tom Fogal <tfogal@alumni.unh.edu>
2014-05-16 22:53:30 +0200

    bfd: add test file.
    forgot to add in last commit...

---
+++ bfd/symtab_test.go
@@ -0,0 +1,18 @@
+package bfd
+
+import "testing"
+
+func TestReadSymbols(t *testing.T) {
+  descriptor, err := OpenR("../testprograms/dimensional", "")
+  if err != nil {
+    t.Fatalf("error opening bfd: %v\n", err)
+  }
+  symbols := Symbols(descriptor)
+
+  if err := Close(descriptor) ; err != nil {
+    t.Fatalf("could not close bfd file: %v\n", err)
+  }
+  if len(symbols) < 20 {
+    t.Fatalf("symbol reading broken?  only read %d symbols.", len(symbols))
+  }
+}
