09dc141edb54cacdfbe5a6acf9f7189be606d8c0
Tom Fogal <tfogal@alumni.unh.edu>
2014-09-09 23:34:48 -0600

    add test for a debug type

---
+++ dbgtype_test.go
@@ -0,0 +1,19 @@
+package main
+import "testing"
+
+func init() {
+  globals = CmdGlobal{"testprograms/dimensional"}
+}
+
+func TestDimsType(t *testing.T) {
+  dims, err := dbg_gvar_basetype(globals.program, 0x602070)
+  if err != nil {
+    t.Fatalf("func failed: %v\n", err)
+  }
+  if dims.ByteSize != 8 {
+    t.Fatalf("byte size %v instead of 8\n", dims.ByteSize)
+  }
+  if dims.Name != "long unsigned int" { // aka size_t
+    t.Fatalf("base type not 'long unsigned int': %v\n", dims.Name)
+  }
+}
