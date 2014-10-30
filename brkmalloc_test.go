342ebc246b3ce1fd0e0bd9b3de69434e3c96edbe
Tom Fogal <tfogal@alumni.unh.edu>
2014-10-30 14:27:16 +0100

    add a test for breaking on malloc.

---
+++ brkmalloc_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "errors"
+import "fmt"
+import "log"
+import "testing"
+import "./bfd"
+import "./debug"
+import "github.com/tfogal/ptrace"
+
+func addr_main(program string) uintptr {
+  symbols, err := bfd.Symbols(program)
+  if err != nil {
+    log.Fatalf("err: %v\n", err)
+  }
+
+  symmain := symbol("main", symbols)
+  return symmain.Address()
+}
+
+func startproc(argv []string) (*ptrace.Tracee, error) {
+  inferior, err := ptrace.Exec(argv[0], argv)
+  if err != nil {
+    return nil, err
+  }
+  /* wait for 1 event, i.e. the notification that the child has exec'd */
+  status := <- inferior.Events()
+  if status == nil {
+    inferior.Close()
+    return nil, errors.New("no events?")
+  }
+
+  debug.WaitUntil(inferior, addr_main(argv[0]))
+
+  return inferior, nil
+}
+
+func TestBreakMalloc(t *testing.T) {
+  argv := []string{"./testprograms/pauser", "3"}
+  inferior, err := startproc(argv)
+  if err != nil { t.Fatalf("%v\n", err) }
+
+  symbols, err := bfd.SymbolsProcess(inferior)
+  if err != nil { t.Fatalf("%v\n", err); }
+
+  malloc := bfd.Symbol{}
+  for _, sym := range symbols {
+    if sym.Name() == "malloc" { malloc = sym }
+  }
+  fmt.Printf("malloc is at 0x%x\n", malloc.Address())
+}
