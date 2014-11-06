1f5e679d17d77d1779ca864ef041ffa7f2b2b240
Tom Fogal <tfogal@alumni.unh.edu>
2014-11-06 18:32:31 +0100

    testprograms: fix indirect hopefully force allocations.

--- testprograms/indirect.cc
+++ testprograms/indirect.cc
@@ -6,14 +6,18 @@
 
 int
 main(int argc, char* argv[]) {
+  uint16_t* ptr = nullptr;
+  uint32_t* v = nullptr;
   if(argc >= 2) {
-    uint16_t* ptr = new uint16_t[atoi(argv[1])];
+    ptr = new uint16_t[atoi(argv[1])];
     std::cout << "got ptr: " << ptr << std::endl;
   }
   std::cout << "pid: " << (long)getpid() << std::endl;
   if(argc >= 2) {
-    uint32_t* v = new uint32_t[atoi(argv[1])];
+    v = new uint32_t[atoi(argv[1])];
     std::cout << "got v: " << v << std::endl;
   }
+  delete[] ptr;
+  delete[] v;
   return 0;
 }
