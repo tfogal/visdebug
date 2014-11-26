b490bfef456a8c8dd4e390ddfc04a34abab43c09
Tom Fogal <tfogal@alumni.unh.edu>
2014-11-26 11:51:30 +0100

    python: just some test code to demonstrate it working.
    it just tries to execute a "test.py" script on startup, and
    (hopefully) doesn't cause any memory leaks.

--- python.go
+++ python.go
@@ -3,13 +3,19 @@
 package main
 
 import(
+  "errors"
+  "unsafe"
   "./msg"
   "github.com/sbinet/go-python"
   "github.com/tfogal/ptrace"
+  "github.com/tfogal/gonpy"
 )
 
 type Python struct {
   BaseEvent
+  module *python.PyObject
+  dict *python.PyObject
+  data *python.PyObject
 }
 
 var pyc = msg.StdChan()
@@ -23,6 +29,44 @@ func (p *Python) Setup(inferior *ptrace.Tracee) error {
     return err
   }
 
+  var err error
+  p.module, err = python.Py_InitModule("simulation")
+  if err != nil {
+    return err
+  }
+
+  p.dict = python.PyModule_GetDict(p.module)
+  if p.dict == nil {
+    return errors.New("newly-created module has no dict?")
+  }
+
+  if i := python.PyRun_SimpleString("import simulation") ; i != 0 {
+    return errors.New("could not import module we created")
+  }
+  if i := python.PyRun_SimpleString("import numpy") ; i != 0 {
+    return errors.New("could not import numpy.")
+  }
+
+  gonpy.Import()
+
+  // just hacks to test out if it works
+  dims := []int{2,2,4}
+  buf := make([]float32, 16)
+  buf[0] = 42.424242
+  p.data, err = gonpy.Array_SimpleNewFromData(uint(len(dims)), dims,
+                                              gonpy.NPY_FLOAT,
+                                              unsafe.Pointer(&buf[0]))
+  if err != nil {
+    return err
+  }
+  str := python.PyString_FromString("testing")
+  python.PyDict_SetItem(p.dict, str, p.data)
+  str.Clear()
+
+  if err := python.PyRun_SimpleFile("test.py") ; err != nil {
+    return err
+  }
+
   pyc.Trace("Initialized.\n")
 
   return nil
@@ -32,6 +76,11 @@ func (p *Python) Close(inferior *ptrace.Tracee) error {
   if err := python.Finalize() ; err != nil {
     return err
   }
+
+  python.PyDict_Clear(p.dict)
+  p.dict.Clear()
+  p.module.Clear()
+  p.data.Clear()
 
   pyc.Trace("Finalized.\n")
   return nil
